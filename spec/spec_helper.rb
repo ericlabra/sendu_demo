eabdbfb393f92ab3d3b400dd0c3e1142bf31134b
ericlabra <eric.labra.reyes@gmail.com>
2026-03-04 18:11:13 -0300

    chore(Rspec): add webmock and filters for coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,6 +30,15 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    add_filter '/spec/'
+    add_filter '/channels/'
+    add_filter '/mailers/'
+    add_filter '/jobs/application_job.rb' # Job base not used
+    add_filter '/controllers/application_controller.rb' # Controller base not used
+  end
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
